013b88ff688bdc6b28ac6fb97474106d4a3f322a
abhishek Vaghela <abhishek.v@simformsolutions.com>
2023-05-09 19:07:51 +0530

    pagination added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import ReactPaginate from "react-paginate";
 import "./App.css";
 // import Access from "./components/Access";
 import Card from "./components/Card";
@@ -8,12 +9,36 @@ import Table from "./components/Table";
 
 function App() {
   // const data = DummyData;
-
+  const handlePageClick = (data) => {
+    console.log(data);
+  };
   return (
-    <div className="main_table">
-      <Table />
-      <Card />
-    </div>
+    <>
+      <div className="main_table">
+        <Table />
+        <Card />
+      </div>
+      {/* <ReactPaginate
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={3}
+        marginPagesDisplayed={2}
+        pageCount={6}
+        previousLabel="< previous"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="pagination"
+        activeClassName="active"
+        breakLabel="..."
+        renderOnZeroPageCount={null}
+      /> */}
+    </>
   );
 }
 
